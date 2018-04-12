83964b5f8c43a77ef09d9a066b591d7b49f3826c
ryand88 <ryand8@hotmail.com>
2018-04-12 17:12:00 -0600

    start/stop button

--- src/containers/Clockin/Clockin.jsx
+++ src/containers/Clockin/Clockin.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import ClockCounter from '../../components/ClockCounter/ClockCounter';
+import Button from '../../components/UI/Button/Button';
 
 class Clockin extends Component {
   state = {
     startTime: new Date(),
     duration: new Date(null),
     test: 1,
-    timerActive: true
-  }
-
-  componentDidMount() {
-    this.runTimer();
+    timerActive: false
   }
 
   componentDidUpdate() {
@@ -25,12 +22,22 @@ class Clockin extends Component {
     }), 1000);
   }
 
+  handleToggleTimer = () => {
+    this.setState((prevState) => ({
+      timerActive: !this.state.timerActive
+    }))
+  }
+
   render() {
     return (
       <div>
         <ClockCounter
           duration={this.state.duration.toISOString()}
           running={this.state.timerActive} />
+        <Button
+          clicked={this.handleToggleTimer}
+          warning={this.state.timerActive}>
+          {this.state.timerActive ? "Stop" : "Start"}</Button>
       </div>
     )
   }
