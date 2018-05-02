88706b3ed8d4ddbf7de1b37f1a39ff325dcf2c12
ryand88 <ryand8@hotmail.com>
2018-05-01 20:16:24 -0600

    test2

---
+++ src/components/Clock/AmPm/AmPm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AmPm.css';
+
+const ampm = (props) => {
+    return (
+        <span className="ampm">
+            <div className={props.am ? 'selected-ampm' : ''}>AM</div>
+            <div className={props.am ? '' : 'selected-ampm'}> PM</div>
+        </span>
+    )
+}
+
+export default ampm
